6112da9ac61bb98760f515681deafb2158577395
Владислав <lapin.vlad01@gmail.com>
2024-06-07 23:01:14 +0700

    Исправлена ошибка, что при создании карточка выходит за пределы поля

--- components/GameField/index.js
+++ components/GameField/index.js
@@ -18,12 +18,27 @@ const GameField = ({pagePath}) => {
   const handleClick = (event) => {
     if (event.currentTarget === event.target) {
       const containerRect = event.currentTarget.getBoundingClientRect();
-      const x = event.clientX - containerRect.left;
-      const y = event.clientY - containerRect.top;
+
+      const sizeBlock = Math.max(containerRect.width * 0.05, 30);//размеры карточки (только ширина, потому что квадрат)
+      
+      //координаты нажатия
+      let x = event.clientX - containerRect.left;
+      let y = event.clientY - containerRect.top;
+      
+      //если карточка выходит за игровое поле сместить её, чтобы поместилась
+      if (containerRect.height - y < sizeBlock) {
+        y = containerRect.height - sizeBlock;
+      }
+
+      if (containerRect.width - x < sizeBlock) {
+        x = containerRect.width - sizeBlock;
+      }
+
       const uniqueId = uuidv4();
+      console.log(containerRect);
+
       const newZIndex = Math.max(...zIndex, 0) + 1;
     setCards([...cards, {id:uniqueId, x, y }]);
-    // setDlina(dlina+1);
     setZIndexState([...zIndex, newZIndex]);
     // setZIndexState([...zIndex, cards.length]);
     }
@@ -36,8 +51,6 @@ const GameField = ({pagePath}) => {
       return newZIndexState;
     });
   };
-
-
 
   return (
     <div className={styles.container} onMouseDown={handleClick}>
